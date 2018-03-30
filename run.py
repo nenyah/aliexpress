6246cec674e6d74f8f1317ff1f2fc71d9a9bc208
steven <steven@nenyah.com>
2018-03-30 14:45:35 +0800

    aliexpess spider

---
+++ run.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+# @Author: Steven 
+# @Date: 2018-03-23 14:08:10 
+# @Last Modified by: Steven 
+# @Last Modified time: 2018-03-23 14:08:58 
+# @file: run.py
+from scrapy.cmdline import execute
+execute("scrapy crawl products".split())
